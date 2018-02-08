ba625143092756e3a34cfab80065e210c19b92bb
Ubuntu <ubuntu@ip-172-26-9-14.us-west-2.compute.internal>
2018-02-08 18:05:56 +0000

    Changed ZigBee valid read to use JSON string instead of ComplianceID

--- TestZigBeeValid.sh
+++ TestZigBeeValid.sh
@@ -1,1 +1,1 @@
-python MqttProxyUsCmd.py  --u DemoUser1 --p ZigBee09 --gwid 0x1111 --rw r --contract ZigBeeDeviceCompliance --func Valid --param '["0x929f5eb7b99a73e6cfd1e249cd9396244054d60e"]' --timeout 30
+python MqttProxyUsCmd.py  --u DemoUser1 --p ZigBee09 --gwid 0x1111 --rw r --contract ZigBeeDeviceCompliance --func Valid --param '[{"Cert":"ZigBee3","Name":"Acme","Model":"Boomerang","HwVer":"1.0","FwVer":"2.0"}]' --timeout 30
