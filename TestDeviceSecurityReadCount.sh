02c0797f7f27794c9755e6378508d668c0bc491a
Ubuntu <ubuntu@ip-172-26-9-14.us-west-2.compute.internal>
2018-02-08 22:17:44 +0000

    Added DeviceSecurity contract

---
+++ TestDeviceSecurityReadCount.sh
@@ -0,0 +1,1 @@
+python MqttProxyUsCmd.py  --u DemoUser1 --p ZigBee09 --gwid 0x1111 --rw r --contract DeviceSecurity --func ReadCount --param '["0xa8f77fc608378ed9961d71ac34fe23279a7c21e1"]' --timeout 30
--- TestZigBeeRead.sh
+++
@@ -1,1 +0,0 @@
-python MqttProxyUsCmd.py  --u DemoUser1 --p ZigBee09 --gwid 0x1111 --rw r --contract ZigBeeDeviceCompliance --func Read --param '["0x929f5eb7b99a73e6cfd1e249cd9396244054d60e"]' --timeout 30
